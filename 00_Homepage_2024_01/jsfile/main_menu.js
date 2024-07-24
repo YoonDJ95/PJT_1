16484686d06798ed905133af5b60d18b93a8b9c5
YoonDJ95 <meslit@naver.com>
2024-07-24 18:48:34 +0900

    Add files via upload

---
+++ 00_Homepage_2024_01/jsfile/main_menu.js
@@ -0,0 +1,12 @@
+$(" #kor_book ").click(function () {
+  $(" #s-d ").toggleClass('show');
+})
+$(" #eng_book ").click(function () {
+  $(" #s-d2 ").toggleClass('show');
+})
+$(" #goods_book ").click(function () {
+  $(" #s-d3 ").toggleClass('show');
+})
+$(" #login_space ").click(function () {
+  $(" #s-d4 ").toggleClass('show');
+})
